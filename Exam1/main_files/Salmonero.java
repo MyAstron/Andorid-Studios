2a2eaa740069db738adbf99cde2bdda07076a28f
My ᗩSTᖇOᑎ ☾ ᒍS <78877006+MyAstron@users.noreply.github.com>
2022-03-10 23:22:42 -0600

    Add Files

---
+++ Exam1/main_files/Salmonero.java
@@ -0,0 +1,37 @@
+package com.example.examen_base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Salmonero extends AppCompatActivity {
+
+    /* Declarar Variables */
+    Button btnRegresar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salmonero);
+
+        /* Asignar Relacion con el Grafico */
+        btnRegresar = (Button) findViewById(R.id.btnRegresar3);
+
+        /* Activar al Momento del Click */
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Crear Objeto tipo Intent
+                Intent cambio = new Intent(getApplication(), MainActivity.class);
+
+                /* Ejecutar el Cambio de Pantalla */
+                startActivity(cambio);
+            }
+        });
+    }
+}
