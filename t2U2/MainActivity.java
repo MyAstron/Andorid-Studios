37f3c71fdb1862a8c9b8ae017405be24b0aab729
My ᗩSTᖇOᑎ ☾ ᒍS <78877006+MyAstron@users.noreply.github.com>
2022-04-27 16:08:18 -0600

    Re post t2U2

--- t2U2/MainActivity.java
+++ t2U2/MainActivity.java
@@ -1,5 +1,3 @@
-## <img src="https://i.ibb.co/VSvC7pw/support-emoji-3.png" height="30"> Configuracion del Programa
-```java
 package com.example.guia2u2;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,4 +49,3 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
-```
