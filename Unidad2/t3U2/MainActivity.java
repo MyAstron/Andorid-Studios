7f942cc7005c3deb2d436a932cea0bb1b92527cc
My ᗩSTᖇOᑎ ☾ ᒍS <78877006+MyAstron@users.noreply.github.com>
2022-07-07 07:15:37 -0600

    Compact `Unidad2`

--- Unidad2/t3U2/MainActivity.java
+++ Unidad2/t3U2/MainActivity.java
@@ -1,5 +1,3 @@
-## <img src="https://i.ibb.co/VSvC7pw/support-emoji-3.png" height="30"> Configuracion del  `activity_main`
-```java
 package com.example.guia3u2;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,4 +41,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-```
